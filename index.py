5fe805173a3cfacbe626baa77703ff0ed26deed7
宋军辉 <songjunhui@zycfcinc.com>
2020-05-12 11:30:31 +0800

    add index  in demo

---
+++ index.py
@@ -0,0 +1,17 @@
+'''
+File Name:index
+Author:SongHao
+Email:5868037@qq.com
+Wechat:songhao8080
+Blog:https://www.168seo.cn
+公众号:zeropython
+date:2020/5/12
+'''
+
+import requests
+
+
+if __name__ == '__main__':
+    url = "http://www.baidu.com"
+    r = requests.get(url)
+    print(r.url)
