e8887a0f6e6defe05b3280fa9e5682daea8083f3
宋军辉 <songjunhui@zycfcinc.com>
2020-05-12 11:34:28 +0800

    add index  in master

---
+++ index.py
@@ -0,0 +1,16 @@
+'''
+File Name:index
+Author:SongHao
+Email:5868037@qq.com
+Wechat:songhao8080
+Blog:https://www.168seo.cn
+公众号:zeropython
+date:2020/5/12
+'''
+import pandas as pd
+import requests
+
+if __name__ == '__main__':
+    url = "htttfdsfdsfds"
+    r = requests.get(url)
+
